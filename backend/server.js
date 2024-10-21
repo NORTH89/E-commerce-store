a93caef6b8c74d39591aff019b799f7729414edf
NORTH89 <nordintata0@gmail.com>
2024-10-21 22:47:52 +0100

    Added express server and connected to MongoDB database.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import authRoutes from "./routes/AuthRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-
+import productsRoutes from "./routes/ProductRoutes.js";
 
 
 dotenv.config();
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allows you to parse the body of the request
 app.use(cookieParser()); // allows you to parse cookies
 
-
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productsRoutes);
 
 app.listen(PORT, () => {
 console.log("Server is running on http://localhost:" + PORT);
