ea8769a21e0e97bca96290ca420e334a1a89c84d
NORTH89 <nordintata0@gmail.com>
2024-10-23 12:56:24 +0100

    Implement authentication, product, and cart routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/AuthRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import productsRoutes from "./routes/ProductRoutes.js";
-
+import cartRoutes from "./routes/cartRoutes.js";
 
 dotenv.config();
 
@@ -17,8 +17,12 @@ app.use(cookieParser()); // allows you to parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productsRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
 console.log("Server is running on http://localhost:" + PORT);
 connectDB();
 });
+/*************  ✨ Codeium Command ⭐  *************/
+const gitMessage = "Implement authentication, product, and cart routes";
+/******  3d8d0570-5807-4455-a059-cd5134cb37ff  *******/
