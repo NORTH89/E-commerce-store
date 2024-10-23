3ba572fa2c574063f4ea6062c1f27c70b22fd0dd
NORTH89 <nordintata0@gmail.com>
2024-10-23 12:51:48 +0100

    Add CRUD operations for cart products with route protection

---
+++ backend/routes/CartRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from "../controllers/cartController.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.put("/", protectRoute, updateQuantity);
+router.delete("/:id", protectRoute, removeAllFromCart);
+
+export default router
+
