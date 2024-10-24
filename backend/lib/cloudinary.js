4fb2b1f80b1b9838e954831add534bddfea74251
NORTH89 <nordintata0@gmail.com>
2024-10-24 16:14:11 +0100

    cloudinary configuration for hashing password

--- backend/lib/cloudinary.js
+++ backend/lib/cloudinary.js
@@ -1,8 +1,10 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+
+// cloudinary configuration for hashing password
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
