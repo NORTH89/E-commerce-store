ddf9ed3af1f665f7f4fec3cd0a05867cbefab037
NORTH89 <nordintata0@gmail.com>
2024-10-31 20:12:00 +0100

    Store for managing the cart items and calculations

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -55,8 +55,8 @@ export const useCartStore = create((set, get) => ({
 				const existingItem = prevState.cart.find((item) => item._id === product._id);
 				const newCart = existingItem
 					? prevState.cart.map((item) =>
-							item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
-					  )
+						item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
+					)
 					: [...prevState.cart, { ...product, quantity: 1 }];
 				return { cart: newCart };
 			});
